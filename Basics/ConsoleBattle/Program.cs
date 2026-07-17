3beb1779ede5ff2430c521054db5fdbf84c35d73
Rampie <rampie.official@gmail.com>
2026-07-17 15:48:59 +0300

    Crit + mana update

--- Basics/ConsoleBattle/Program.cs
+++ Basics/ConsoleBattle/Program.cs
@@ -1,9 +1,11 @@
 ﻿using static System.Net.Mime.MediaTypeNames;
 
-int playerHP = 100;
-int enemyHP = 80;
+int playerHP = 150, maxPlayerHP = 150;
+int playerMana = 50, maxPlayerMana = 50;
+int enemyHP = 120;
 Random rand = new Random();
 bool isTrue = true;
+bool isCrit = false;
 
 bool turn = rand.Next(2) > 0;
 string attackerName;
@@ -20,7 +22,8 @@ while (isTrue)
     if (turn == true)
     {
         int action;
-        Console.WriteLine("\n\n-- Ваш ход --\n1. Атака.\n2. Лечение.");
+        
+        Console.WriteLine("\n\n-- Ваш ход --\n1. Атака.\n2. Лечение.\n3. Огненный шар.");
         Console.Write("Выберите действие: ");
         if (int.TryParse(Console.ReadLine(), out action))
         {
@@ -30,6 +33,7 @@ while (isTrue)
         {
             Console.WriteLine("Введено неверное значение! Повторите попытку");
         }
+        
         switch (action)
         {
             case 1:
@@ -38,10 +42,25 @@ while (isTrue)
                     turn = false;
                     break;
                 }
-            case 2: 
+            case 2:
                 {
                     playerHeal();
                     turn = false;
+                    break;
+                }
+            case 3:
+                {
+                    if (playerMana >= 20)
+                    {
+                        playerCastFireball();
+                        turn = false;
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Недостаточно маны!");
+                        break;
+                    }
                     break;
                 }
             default:
@@ -51,13 +70,16 @@ while (isTrue)
                 }
 
         }
+        
     }
     else
     {
         enemyAttack();
+        playerMana = Math.Min(playerMana + 5, maxPlayerMana);
         turn = true;
     }
-    if (playerHP <=0)
+
+    if (playerHP <= 0)
     {
         Console.WriteLine("\n\nВы проиграли!");
         break;
@@ -67,26 +89,51 @@ while (isTrue)
         Console.WriteLine("\n\nВы победили!");
         break;
     }
-    
-    Console.WriteLine($"\nХп игрока: {playerHP}. Хп соперника: {enemyHP}");
+
+    Console.WriteLine($"\nХп игрока: {playerHP}. Остаток маны: {playerMana}\n Хп соперника: {enemyHP}");
 }
 
 
 void enemyAttack()
 {
+    isCrit = rand.Next(101) <= 20;
     int damage = rand.Next(7, 30);
+    if (isCrit == true)
+    {
+        damage *= 2;
+        Console.WriteLine("КРИТИЧЕСКИЙ УДАР!");
+    }
     playerHP -= damage;
     Console.WriteLine($"\nПротивник наносит {damage} урона!");
 }
 void playerAttack()
 {
+    isCrit = rand.Next(101) <= 20;
     int damage = rand.Next(10, 20);
+    if (isCrit == true)
+    {
+        damage *= 2;
+        Console.WriteLine("КРИТИЧЕСКИЙ УДАР!");
+    }
     enemyHP -= damage;
     Console.WriteLine($"Вы нанесли {damage} урона противнику!");
 }
 void playerHeal()
 {
     int heal = rand.Next(5, 25);
-    playerHP += heal;
+    playerHP = Math.Min(playerHP + heal, maxPlayerHP);
     Console.WriteLine($"Вы восстановили {heal} единиц здоровья!");
+}
+void playerCastFireball()
+{
+    isCrit = rand.Next(101) <= 20;
+    int damage = rand.Next(15, 30);
+    playerMana -= 20;
+    if (isCrit == true)
+    {
+        damage *= 2;
+        Console.WriteLine("КРИТИЧЕСКИЙ УДАР!");
+    }
+    enemyHP -= damage;
+    Console.WriteLine($"Вы нанесли {damage} урона противнику огненным шаром!");
 }
